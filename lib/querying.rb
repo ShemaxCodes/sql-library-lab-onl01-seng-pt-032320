fdff39e15ce84966174ec56e33b856d011875daf
Jashema Panapa <jcpanapa@gmail.com>
2020-05-28 16:33:17 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,6 @@ end
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name FROM authors
 JOIN subgenres ON authors.id = subgenres.id;"
-
 end
 
 def select_series_title_with_most_human_characters
